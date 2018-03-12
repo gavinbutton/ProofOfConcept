1fc96a64d4a50f32487fb2b084206af741f46f8f
Gavin Button <gavin.button@ge.com>
2018-03-12 12:34:44 +0000

    Intial Version

---
+++ FloatingWindowRoutedCommandTest/FloatingWindowRoutedCommandTest/MainContentView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FloatingWindowRoutedCommandTest
+{
+    /// <summary>
+    /// Interaction logic for MainContentView.xaml
+    /// </summary>
+    public partial class MainContentView : UserControl
+    {
+        private PaneViewModel vm = new PaneViewModel();
+        public MainContentView()
+        {
+            InitializeComponent();
+            this.paneC.DataContext = this.vm;
+        }
+        
+        private void OnExecute(object sender, ExecutedRoutedEventArgs e)
+        {
+            var message = $"Executed {DateTime.Now}";
+            System.Diagnostics.Debug.WriteLine(message);
+            this.vm.AddLog(message);
+        }
+
+        private void CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
---
+++ FloatingWindowRoutedCommandTest/FloatingWindowRoutedCommandTest/PaneViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatingWindowRoutedCommandTest
+{
+    class PaneViewModel : INotifyPropertyChanged
+    {
+        private StringBuilder log = new StringBuilder();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void AddLog(string message)
+        {
+            this.log.AppendLine(message);
+            NotifyPropertyChanged("Log");
+        } 
+
+        public string Log
+        {
+            get
+            {
+                return log.ToString();
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
